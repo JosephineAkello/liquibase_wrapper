9b0210c074606f885f83b2f460848e3c54ebf0a2
Maureen Josephine <m.akelo@carepay.com>
2021-08-24 22:06:23 +0300

    initial commit

---
+++ src/main/java/liquibase_wrapper/liquibase_wrapper/LiquibaseWrapperApplication.java
@@ -0,0 +1,13 @@
+package liquibase_wrapper.liquibase_wrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseWrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseWrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/liquibase_wrapper/liquibase_wrapper/LiquibaseWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package liquibase_wrapper.liquibase_wrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
